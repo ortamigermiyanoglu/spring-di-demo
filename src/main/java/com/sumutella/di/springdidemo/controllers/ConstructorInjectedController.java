02777614f4ae6abbc88c5e4320fd539329f80a46
Semir Umut Kurt <fatih.ek96@hotmail.com>
2019-10-25 22:18:34 +0300

    Update ConstructorInjectedController.java

--- src/main/java/com/sumutella/di/springdidemo/controllers/ConstructorInjectedController.java
+++ src/main/java/com/sumutella/di/springdidemo/controllers/ConstructorInjectedController.java
@@ -1,9 +1,21 @@
 package com.sumutella.di.springdidemo.controllers;
 
+import com.sumutella.di.springdidemo.services.GreetingService;
+import com.sumutella.di.springdidemo.services.GreetingServiceImpl;
+
 /**
  * @author sumutella
  * @time 8:18 PM
  * @since 10/25/2019, Fri
  */
 public class ConstructorInjectedController {
+    private GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingServiceImpl greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
 }
